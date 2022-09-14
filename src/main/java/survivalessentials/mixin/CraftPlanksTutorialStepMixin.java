9e9dda9c978fa6990126e01bd52923835cbd4414
Wendall Cada <wendallc@83864.com>
2022-09-14 13:54:04 -0700

    Skip plank crafting tutorial.

---
+++ src/main/java/survivalessentials/mixin/CraftPlanksTutorialStepMixin.java
@@ -0,0 +1,19 @@
+package survivalessentials.mixin;
+
+import net.minecraft.client.tutorial.CraftPlanksTutorialStep;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CraftPlanksTutorialStep.class)
+public abstract class CraftPlanksTutorialStepMixin {
+
+    @Inject(method = "tick",
+        at = @At("HEAD"), cancellable = true)
+    private void injectTick(CallbackInfo info) {
+        info.cancel();
+    }
+
+}
