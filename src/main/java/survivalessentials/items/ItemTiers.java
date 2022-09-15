1998f4f8e5b3119797e4243fd88d44cf92ad7af4
Wendall Cada <wendallc@83864.com>
2022-09-15 09:55:58 -0700

    Change durability for material tiers.

--- src/main/java/survivalessentials/items/ItemTiers.java
+++ src/main/java/survivalessentials/items/ItemTiers.java
@@ -11,9 +11,9 @@ public class ItemTiers {
         .setAttackDamage(0.5F).setRepairMat(Items.FLINT);
     public static final Tier STONE_TIER = new ItemTier().setMaxUses(5).setEfficiency(1.5F)
         .setAttackDamage(0.5F).setRepairMat(Items.COBBLESTONE);
-    public static final Tier IRON_TIER = new ItemTier().setMaxUses(100).setEfficiency(1.5F)
+    public static final Tier IRON_TIER = new ItemTier().setMaxUses(320).setEfficiency(1.5F)
         .setAttackDamage(0.0F).setRepairMat(Tags.Items.INGOTS_IRON);
-    public static final Tier DIAMOND_TIER = new ItemTier().setMaxUses(300).setEfficiency(1.5F)
+    public static final Tier DIAMOND_TIER = new ItemTier().setMaxUses(640).setEfficiency(1.5F)
         .setAttackDamage(0.0F).setRepairMat(Items.DIAMOND);
     public static final Tier NO_TIER = new ItemTier().setMaxUses(1).setEfficiency(0.0F)
             .setAttackDamage(0.0F);
