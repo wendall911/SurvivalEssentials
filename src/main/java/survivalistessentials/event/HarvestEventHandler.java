c682d62c5178b8c7064bce20a15b07befd5e7874
Wendall Cada <wendallc@83864.com>
2024-09-01 09:32:51 -0700

    Check if other mod is using FakePlayer and allow harvest.

--- src/main/java/survivalistessentials/event/HarvestEventHandler.java
+++ src/main/java/survivalistessentials/event/HarvestEventHandler.java
@@ -21,6 +21,7 @@ import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModList;
 import net.neoforged.neoforge.common.Tags;
+import net.neoforged.neoforge.common.util.FakePlayer;
 import net.neoforged.neoforge.event.TagsUpdatedEvent;
 import net.neoforged.neoforge.event.entity.ProjectileImpactEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
@@ -54,6 +55,7 @@ public class HarvestEventHandler {
         final BlockPos pos = event.getPos();
         final BlockState state = level.getBlockState(pos);
         final Player player = event.getPlayer();
+        if (player instanceof FakePlayer) return;
         final ToolType expectedToolType = HarvestBlock.BLOCK_TOOL_TYPES.getOrDefault(state.getBlock(), ToolType.NONE);
         boolean cancel = false;
         boolean alwaysBreakable = state.is(TagManager.Blocks.ALWAYS_BREAKABLE) ||
@@ -135,7 +137,7 @@ public class HarvestEventHandler {
         final Player player = event.getEntity();
         final BlockState state = event.getTargetBlock();
 
-        if (!player.isCreative()) {
+        if (!(player instanceof FakePlayer) && !player.isCreative()) {
             final ItemStack handStack = getHandStack(player, state);
             final boolean correctTool = ItemUse.isCorrectTool(state, player, handStack);
             final ToolType expectedToolType = HarvestBlock.BLOCK_TOOL_TYPES.getOrDefault(state.getBlock(), ToolType.NONE);
@@ -164,6 +166,7 @@ public class HarvestEventHandler {
         final Player player = event.getEntity();
         final Optional<BlockPos> pos = event.getPosition();
 
+        if (player instanceof FakePlayer) return;
         if (pos.isEmpty()) return;
 
         final Level level = player.level();
